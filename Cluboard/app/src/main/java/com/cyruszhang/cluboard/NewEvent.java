340c6f57691d83d60d7f3fc795b314bc0b0ba39a
Anton Qiu <qiuchangtong@gmail.com>
2016-02-12 23:04:28 -0800

    saveinback

--- Cluboard/app/src/main/java/com/cyruszhang/cluboard/NewEvent.java
+++ Cluboard/app/src/main/java/com/cyruszhang/cluboard/NewEvent.java
@@ -109,6 +109,7 @@ public class NewEvent extends AppCompatActivity {
                     relationACL.setPublicReadAccess(true);
                     relationACL.setPublicWriteAccess(true);
                     newRelation.setACL(relationACL);
+                    newRelation.saveInBackground();
                     newEvent.saveInBackground(new SaveCallback() {
                         @Override
                         public void done(ParseException e) {
