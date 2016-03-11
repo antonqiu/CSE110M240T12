21068b7760dcbc66095e02d4c081eb0e7e917d7d
Xinyuan Zhang <xiz016@ucsd.edu>
2016-03-11 01:45:14 -0800

    login test in progresss

--- Cluboard/app/src/androidTest/java/com/cyruszhang/cluboard/LoginTest.java
+++ Cluboard/app/src/androidTest/java/com/cyruszhang/cluboard/LoginTest.java
@@ -5,7 +5,12 @@ import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.PerformException;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.util.HumanReadables;
+import android.support.test.espresso.util.TreeIterables;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -13,6 +18,7 @@ import com.cyruszhang.cluboard.activity.Home;
 import com.cyruszhang.cluboard.activity.Login;
 import com.parse.ui.ParseLoginFragment;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -24,6 +30,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -31,6 +38,9 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.util.Log;
+import android.view.View;
+
+import java.util.concurrent.TimeoutException;
 
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
@@ -51,16 +61,16 @@ public class LoginTest {
         onView(withId(com.parse.ui.R.id.login_password_input)).perform(typeText("123456"));
         closeSoftKeyboard();
         onView(withId(com.parse.ui.R.id.parse_login_button)).perform(click());
-    }
-        /*
+
+        onView(isRoot()).perform(waitId(R.id.home_upcoming_event_icon, 10000));
         intended(hasComponent(Home.class.getName()));
         onView(withContentDescription("Open navigation drawer")).perform(click());
         onView(withText("Logout")).perform(click());
         intended(hasComponent("MainActivity"));
         Intents.release();
+    }
 
-
-
+/*
 
 
 
@@ -73,7 +83,47 @@ public class LoginTest {
         Intents.release();
 
     }*/
+    /** Perform action of waiting for a specific view id. */
+    public static ViewAction waitId(final int viewId, final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
 
+            @Override
+            public String getDescription() {
+                return "wait for a specific view with id <" + viewId + "> during " + millis + " millis.";
+            }
+
+            @Override
+            public void perform(final UiController uiController, final View view) {
+                uiController.loopMainThreadUntilIdle();
+                final long startTime = System.currentTimeMillis();
+                final long endTime = startTime + millis;
+                final Matcher<View> viewMatcher = withId(viewId);
+
+                do {
+                    for (View child : TreeIterables.breadthFirstViewTraversal(view)) {
+                        // found view with required ID
+                        if (viewMatcher.matches(child)) {
+                            return;
+                        }
+                    }
+
+                    uiController.loopMainThreadForAtLeast(50);
+                }
+                while (System.currentTimeMillis() < endTime);
+
+                // timeout happens
+                throw new PerformException.Builder()
+                        .withActionDescription(this.getDescription())
+                        .withViewDescription(HumanReadables.describe(view))
+                        .withCause(new TimeoutException())
+                        .build();
+            }
+        };
+    }
 }
 
 
